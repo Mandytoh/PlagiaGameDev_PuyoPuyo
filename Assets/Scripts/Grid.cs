2b1b569adf3d50ae6e9db999bee7ade4ca040913
Mandytoh <mr.mandytoh@gmail.com>
2016-02-20 15:09:26 +0100

    Add sources

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+
+	public int initialLineFilled = 4;
+	public Vector2 size = new Vector2(6, 12);
+	public List<List<Node>> grid = new List<List<Node>>();
+	public Node nodePrefab;
+
+	// Use this for initialization
+	void Start () {
+		for (int y = 0; y < this.size.y; y++) {
+			for (int x = 0; x < this.size.x; x++) {
+				if(y >= this.size.y - this.initialLineFilled){
+					nodePrefab.position = new Vector2(x, y);
+					GameObject node = Instantiate(nodePrefab.gameObject,
+						new Vector3(x, this.size.y - y, 0),
+		            	Quaternion.identity);
+					this.grid[y][x] = nodePrefab;
+					searchSameNodes(node);
+				}
+			}
+		}
+	}
+
+	public Node searchSameNodes( Node node ){
+		Vector2 actualPosition = node.position;
+		for (int y = -1; y <= 1; y++) {
+			for (int x = -1; x <= 1; x++) {
+				if(x != 0 && y != 0){
+					if(this.grid[y][x] != null){
+						Node controlledNode = this.grid[y][x];
+						if(controlledNode.type == node.type){
+							controlledNode.GetComponent<MeshRenderer>().material.color = Color(2,2,2);
+							Debug.Log(controlledNode.position);
+							Debug.Log(controlledNode.type);
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+	public Color[] colors = new Color[]{
+		new Color (1, 0, 0),
+		new Color (0, 1, 0),
+		new Color (0, 0, 1)
+	};
+
+	public string[] types = new string[]{
+		"Cerise",
+		"Pomme",
+		"Chewing gum"
+	};
+
+	public Vector2 position = Vector2.zero;
+	public string type;
+
+	void Start () {
+		int randIndex = Random.Range(0, colors.Length);
+		this.GetComponent<MeshRenderer> ().material.color = colors [randIndex];
+		this.type = types [randIndex];
+	}
+}
